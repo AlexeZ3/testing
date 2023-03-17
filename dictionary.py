830ac9d1487eb3bb449fd2f4f59a83f058375dae
Arretez95 <126123559+AlexeZ3@users.noreply.github.com>
2023-03-16 21:24:52 -0500

    secondtestcommit

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 16 18:55:40 2023
+
+@author: azech
+"""
+
+myself={"name": "Alexe",
+        "age": 27,
+        "hobbies": ["Play video games", "Read", "Workout"]
+        }
+
+print(f'Hello I am {myself["name"]} and I am {myself["age"]}')
+print(f'I have {len(myself["hobbies"])} hobbies!')
